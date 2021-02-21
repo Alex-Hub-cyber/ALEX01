5667778b35fb38292edad974e94c7bf04158b3c8
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-02-21 11:47:08 -0600

    ALEX02
    TAREA DE PROGRAMACIÓN

---
+++ ALEX02/Historial/ALEX01/NEGOCIO/ClsLogin.cs
@@ -0,0 +1,20 @@
+﻿using ALEX01.DOMINIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALEX01.NEGOCIO
+{
+    class ClsLogin
+    {
+
+        Login log = new Login();
+
+       
+     
+
+
+    }
+}
